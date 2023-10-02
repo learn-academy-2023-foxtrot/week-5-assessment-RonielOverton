263faadab156d7af3f9df0809248e27253f2deee
RonielOverton <ronielo86@gmail.com>
2023-10-01 18:05:31 -0700

    week 5 done minus a couple i can't figure out

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,11 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
+// const { describe } = require("yargs")
+
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,36 +20,93 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// describe('codedMessage',()=>{
+// const secretCodeWord1 = "Lackadaisical"
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook"
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric"
+// // Expected output: "3cc3ntr1c"
+
+//     it('returns a string with a coded message',() =>{
+
+//     expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//     expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//     expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+//     })
+// })
+
+// Output  ReferenceError: codedMessage is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Name: codedMessage
+// Input: String
+// Output: string
+// Process: I am going to create a function that takes in a string replace certain characters in the string using .replace().
+// Added a .replace for all the letters that needed changing as a chain.
+// Each .replace needed a <g> for every extra letter and a <i> so it will not be case sensitive. 
+
+// const codedMessage = (str) => {
+//     console.log();
+//    return str.replaceAll(/a/gi, 4 ).replaceAll(/e/gi, 3).replaceAll(/i/gi, 1).replaceAll(/o/gi, 0)
+// }
+
+// Output: Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.213 s, estimated 1 s
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe('onlyContains',() => {
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+    const filterLetterA = "a"
+    // Expected output: ["Mango", "Apricot", "Peach"]
+    const filterLetterE = "e"
+    // Expected output: ["Cherry", "Blueberry", "Peach"]
+
+    it('returns an array of all the words containing that particular letter', () =>{
+
+        expect(onlyContains(filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+
+        expect(onlyContains(filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
+
+//  Output: ReferenceError: onlyContains is not defined
+
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Name: onlyContains
+// Input: array
+// Output: array
+// Process: create a function that takes in an array and returns an array with words that only contain particular letter using .includes() and if array contains words with .filter().
+
+// const onlyContains = (letterToCheck) => {
+//     const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+//     const newArr = fruitArray.filter((fruit) => fruit.toLowerCase().includes(letterToCheck))
+//     // if(fruitArray.includes(arr) === true){
+//     // }
+//     return newArr
+
+// }
+// Output: Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.193 s, estimated 1 s
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
+describe('paintedLadies', () => {
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
 const hand2 = [5, 5, 3, 3, 4]
@@ -52,7 +115,60 @@ const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
+    it('determines whether or not the array is a "full house"', () => {
+        expect(paintedLadies(hand1)).toEqual(true)
+        expect(paintedLadies(hand2)).toEqual(false)
+        expect(paintedLadies(hand3)).toEqual(false)
+        expect(paintedLadies(hand4)).toEqual(true)
+        
+    })
+})
+
+// Output: ReferenceError: paintedLadies is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Name: paintedLadies
+// Input: array
+// Output: boolean value
+// Process: Create a function that takes in a array an checks if there a multiples of two and three using .includes()
+// #### Edit: I had a few approaches for this challenge. All of them failed, I'm calling it on this one....
+
+// const paintedLadies = (arr) => {
+//    const copy = arr.slice()
+//    for(let i = 0; i < arr.length; ){
+//     const el = copy.slice(i, 1)[0]
+//     if(copy.includes(el)){
+//         copy.splice(copy.indexOf(el), 1)
+//         if(copy.includes(el)){
+//             return true
+//         }
+//     } else {
+//         i++
+//     }
+//    }
+//     return false
+// }
+
+// const paintedLadies = (arr1,arr2,arr3,arr4) =>{
+//     const checkFullHouse = (arr) => {
+//         return(
+//             arr.includes(5) && arr.includes(3) &&
+//             (arr.includes(5, 5, 5) || arr.includes(3,3, 3))
+//         )
+//     }
+// const hands = [arr1, arr2, arr3, arr4]
+    // }   
+
+
+    // const paintedLadies = (arr) => {
+    //     arr.sort(arr)
+    //     for(let i = 0; i < arr.length; i++)
+    
+    // return 
+    // };   
+    //  const hasPair = (arr) => {
+    //     for(let i = 0; i < arr.length; i++) 
+            
+    // }
--- interview-questions.md
+++ interview-questions.md
@@ -6,44 +6,60 @@ First, without external resources. Challenge yourself to answer from memory.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions:R.O
 
 1. Why would super be used in a Ruby class?
 
-Your answer:
+Your answer: Super would be used in a Ruby class to bring down properties from a higher class ie: Inheritance 
 
-Researched answer:
+Researched answer: Ruby uses the super keyword to call on the superclass to implement on the current method. Within the body of a method, super acts like a call to the original method.
+The search for a method body starts in the superclass of the object that was found to contain the original method.
+
+Example:
+
+ class Pointer < Dog
+  def initialize(name, breed)
+    super(name)
+    @breed = breed
+  end
+
+  def get_info
+    "#{@name} is a #{@breed}."
+  end
+end
+
+
 
 2. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a way to access data points that are related to each other.
 
-Researched answer:
+Researched answer: A relational database is a type of database that stores and provides access to data points that are related to one another. In a relational database, each row in the table is a record with a unique ID called the key. (foreshadowing)
 
 3. What are primary keys? Why purpose does a primary key serve?
 
-Your answer:
+Your answer: A primary key is a unique id given to a model automatically by rails. This key is for one and one only model.
 
-Researched answer:
+Researched answer: A primary key or keyword is a column in a  relational database table that's distinctive for each record. It's unique to that record like a driver's license number is unique to everyone.
 
 4. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: I cannot remember what the five HTTP verbs are.
 
-Researched answer:
+Researched answer: The five HTTP verbs are Post, Get, Put, Patch and Delete. These correspond to create, read, update, and delete. 
 
 5. STRETCH: What is a JOIN table in SQL?
 
-Your answer:
+Your answer: I'm not sure this is a total shot in the dark but it sounds like some type of inheritance within tables.
 
-Researched answer:
+Researched answer: A JOIN is a SQL clause used to query and access data from multiple tables. JOINS indicate how SQL servers should use data from one table to select rows from another table.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: Model validation is the process that is carried out after Model Training where the trained model is evaluated with a testing data set. 
 
-2. Params:
+2. Params: Parameters define variable elements of a URL path, query parameters, headers, or a request body. You can create parameters for Paths and Path operations in your API definition.
 
-3. API:
+3. API: APIs are mechanisms that enable two software components to communicate with each other using a set of definitions and protocols.
